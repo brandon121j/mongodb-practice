747963e313f4a3228b47a49322bbe90c22d1364e
Brandon <brandon121j@gmail.com>
2021-09-24 22:09:36 -0400

    Addition of router.put route

--- routes/practice/practiceRouter.js
+++ routes/practice/practiceRouter.js
@@ -25,3 +25,15 @@ router.get('/get-practice-by-id/:id', function(req, res) {
         }
     });
 });
+
+router.put('/update-practice-by-id/:id', function(req, res) {
+    practiceController.updatePractice(req.params.id, req.body, function(err, updatedPractice) {
+        if (err) {
+            res
+                .status(500)
+                .json({ message: "Something went wrong!", error: err })
+        } else {
+            res.json({ message: "Successfully updated", updatedPractice })
+        }
+    });
+});
