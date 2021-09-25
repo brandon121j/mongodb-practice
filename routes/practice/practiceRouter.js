9489c3b99d77eb6e26ba2146a2e23988abe08834
Brandon <brandon121j@gmail.com>
2021-09-24 22:36:45 -0400

    Testing

--- routes/practice/practiceRouter.js
+++ routes/practice/practiceRouter.js
@@ -26,6 +26,18 @@ router.get('/get-practice-by-id/:id', function(req, res) {
     });
 });
 
+router.post('/', function(req,res) {
+    practiceController.createPractice(req.body, function(err, createdPractice) {
+        if (err) {
+            res
+                .status(500)
+                .json({ message: "Something went wrong", error: err})
+        } else {
+            res.json({ message: "Item successfully created!", createdPractice })
+        }
+    })
+})
+
 router.put('/update-practice-by-id/:id', function(req, res) {
     practiceController.updatePractice(req.params.id, req.body, function(err, updatedPractice) {
         if (err) {
@@ -45,7 +57,7 @@ router.delete('/delete-practice-by-id/:id', function(req, res) {
                 .status(500)
                 .json({ message: "Something went wrong!", error: err })
         } else {
-            res.json({ message: "Item successfully delted!", deletedItem })
+            res.json({ message: "Item successfully deleted!", deletedItem })
         }
     });
 });
