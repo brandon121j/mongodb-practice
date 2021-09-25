4cd72d6df104cb2bd4a6a788637c7dd35c3aa391
Brandon <brandon121j@gmail.com>
2021-09-24 21:45:02 -0400

    Addition of folders and files

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var practice = require('')
+
+mongoose
+  .connect("mongodb://localhost:27017/practice", {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB CONNECTED");
+  })
+  .catch((e) => {
+    console.log(e);
+  })
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/practice', )
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
